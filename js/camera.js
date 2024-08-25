7c179bd90c352bace3bca6ca33e562c228faf3e2
Dhruba Ghosh <djghosh@cs.washington.edu>
2024-08-25 13:47:34 -0700

    Better camera

--- js/camera.js
+++ js/camera.js
@@ -1,5 +1,5 @@
 class Camera {
-    constructor(target, { speed = 100 }) {
+    constructor(target, { stiffness = 9, damping = 1 }) {
         this.target = target;
         this.renderOrder = 0;
         if (this.target != null) {
@@ -8,18 +8,15 @@ class Camera {
         else {
             this.position = Vector.ZERO;
         }
-        this.speed = speed;
+        this.velocity = Vector.ZERO;
+        this.stiffness = stiffness, this.damping = damping;
     }
     update(game) {
-        if (this.target != null) {
-            let offset = this.target.armature[0].start.sub(this.position);
-            if (offset.norm < this.speed * FRAME_INTERVAL / 1000) {
-                this.position = this.position.add(offset);
-            }
-            else {
-                this.position = this.position.add(offset.normalized().mul(this.speed * FRAME_INTERVAL / 1000));
-            }
-        }
+        let targetPos = (this.target != null) ? this.target.armature[0].start : this.position;
+        this.position = this.position.add(this.velocity.mul(FRAME_INTERVAL / 1000));
+        let pullFactor = targetPos.sub(this.position).mul(this.stiffness);
+        let dampingFactor = this.velocity.mul(-2 * this.damping * Math.sqrt(this.stiffness));
+        this.velocity = this.velocity.add(pullFactor.add(dampingFactor).mul(FRAME_INTERVAL / 1000));
     }
     render(ctx) { }
 }
